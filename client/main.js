d0ea3a2f8a58223ae7bdd392e91d95080d88def5
williamchavula <rumbani@gmail.com>
2022-11-05 20:46:47 -0500

    add simple react components for testing setup

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+
+import HelloWorld from './HelloWorld';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<HelloWorld />);
---
+++ template.js
@@ -0,0 +1,18 @@
+export default () => {
+	return `<!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <title>MERN Skeleton</title>
+      </head>
+      <body>
+        <div id="root></div>
+    
+        <script src="/dist/bundle.js" type="text/javascript"></script>
+      </body>
+    </html>
+    `;
+};
