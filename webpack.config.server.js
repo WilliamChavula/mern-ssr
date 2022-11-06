7f6d21b22f42c8d1ba7f13db51b791863b4edf38
williamchavula <rumbani@gmail.com>
2022-11-05 20:21:32 -0500

    configure webpack for different dev environments

---
+++ webpack.config.server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+// const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const CWD = process.cwd();
+
+const config = {
+	name: 'server',
+	externalsPresets: { node: true },
+	externals: [nodeExternals()],
+	entry: [path.join(CWD, 'server/server.js')],
+	output: {
+		path: path.join(CWD, '/dist/'),
+		filename: 'server.generated.js',
+		libraryTarget: 'commonjs2',
+		publicPath: '/dist/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+};
+
+module.exports = config;
