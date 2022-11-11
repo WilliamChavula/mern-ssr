7a481e92eb6096325e791424846e769eb87578dc
williamchavula <rumbani@gmail.com>
2022-11-11 13:47:52 -0600

    add helper methods to integrate auth API endpoints

---
+++ client/auth/api-auth.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Logger from 'js-logger';
+
+Logger.useDefaults();
+
+const signin = async user => {
+	try {
+		const response = await axios.post('/auth/signin', JSON.stringify(user), {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			withCredentials: true,
+		});
+	} catch (error) {
+		Logger.error(error);
+	}
+};
+
+const signout = async () => {
+	try {
+		const response = await axios.get('/auth/signout/');
+
+		return response.data;
+	} catch (error) {
+		Logger.error(error);
+	}
+};
+
+export { signin, signout };
