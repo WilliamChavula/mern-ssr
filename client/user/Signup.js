1e6dc51ef3ae51c6dbbac6cca749fc946fe448e2
williamchavula <rumbani@gmail.com>
2022-11-12 14:25:49 -0600

    add lexical ordering of imports

--- client/user/Signup.js
+++ client/user/Signup.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { styled } from '@mui/material';
 
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
 import Icon from '@mui/material/Icon';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import { create } from './api-user';
-import { styled } from '@mui/material';
 
 const CardComponent = styled(Card)(({ theme }) => ({
 	maxWidth: 600,
