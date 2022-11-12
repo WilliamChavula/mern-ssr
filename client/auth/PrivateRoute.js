735fcb94d0564dabef7361e5add50ddfab235b2d
williamchavula <rumbani@gmail.com>
2022-11-12 11:13:22 -0600

    refactor to alt solution with Navigate component

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -1,4 +1,4 @@
-import { redirect } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 import { isAuthenticated } from './auth-helper';
 
@@ -6,8 +6,25 @@ import { isAuthenticated } from './auth-helper';
  * @param  {Object} {children} - React Node representing children to render to the tree
  * component will simply check the current user auth state, then redirect to the Signin component if the user is not authenticated
  */
-const PrivateRoute = ({ children }) => {
-	return isAuthenticated() ? children : redirect('/signin');
+const PrivateRoute = () => {
+	const location = useLocation();
+	const hasAuthenticated = isAuthenticated();
+
+	if (!hasAuthenticated)
+		return (
+			<Navigate
+				to='/signin'
+				state={{ from: location }}
+			/>
+		);
+
+	return <Outlet />;
 };
+
+// const PrivateRoute = ({ children }) => {
+// 	const hasAuthenticated = isAuthenticated();
+
+// 	return hasAuthenticated ? children : redirect(to='/signin')
+// }
 
 export default PrivateRoute;
